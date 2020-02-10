8492f9dbedf6d335ff297da2408ff452cfabecc6
pedram katanchi <pedramkatanchi@webmapp.it>
2020-02-10 12:54:39 +0100

    Added style to the wizard dialog

--- shortcodes/dashboard_wizard_button.php
+++ shortcodes/dashboard_wizard_button.php
@@ -3,24 +3,26 @@
 add_action('wp_dashboard_setup', 'webmapp_wizard_dashboard_button');
   
 function webmapp_wizard_dashboard_button() {
-global $wp_meta_boxes;
 
 wp_add_dashboard_widget('custom_help_widget', 'Webmapp wizard', 'wizard_button', 'dashboard', 'normal', 'high');
 }
  
 function wizard_button() {
-    echo '<p>Crea una nuova route:</p>';
-    echo '<button class="button button-primary button-hero load-customize hide-if-no-customize" id="dialogButton">Nuova route</button >';
-    echo '<div id="dialog" title="Basic dialog">';
-    echo do_shortcode("[testAngular]");
-    echo '</div>';
-    ?> 
+    ob_start();
+    ?>
+    <p>Crea una nuova route:</p>
+    <button class="button button-primary button-hero load-customize hide-if-no-customize" id="dialogButton">Nuova route</button>
+    <div id="dialog" title="Basic dialog">
+        <?php echo do_shortcode("[testAngular]"); ?>
+    </div> 
+
     <script>
-        jQuery( function() {
+        jQuery(document).ready(function () {
             jQuery( "#dialog" ).dialog({
                 autoOpen: false, //FALSE if you open the dialog with, for example, a button click
-                title: 'Nuova route',
-                modal: true
+                closeOnEscape: false,
+                modal: true,
+                minWidth: "50%",
             });
 
             // add the onclick handler
@@ -31,4 +33,6 @@ function wizard_button() {
         } );
     </script>
     <?php
+
+    echo ob_get_clean();
 }
