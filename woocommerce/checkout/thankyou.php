9f7c5f11bde851bbfd423ed0f49c8bf3749a770c
pedram katanchi <pedramkatanchi@webmapp.it>
2020-02-21 15:35:53 +0100

    Modified thatnkyou page template IDs

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -70,8 +70,8 @@ defined( 'ABSPATH' ) || exit;
             <?php
                 echo '<div class="tour-general-info" style="display: inline-block;"><p><strong>';
                 echo __('Departure date:' ,'wm-child-verdenatura').' </strong>';
-                echo '<span id="data-partenza">'.$departure_date.'</span></p>';
-				echo '<p id="route-name"><strong>'.__('Route name:','wm-child-verdenatura').'</strong> <a target="_blank" href="'.$routePermalink.'">'.$routName.'</a></p>';
+                echo '<span id="thankyou-data-partenza">'.$departure_date.'</span></p>';
+				echo '<p id="thankyou-route-name"><strong>'.__('Route name:','wm-child-verdenatura').'</strong> <a target="_blank" href="'.$routePermalink.'">'.$routName.'</a></p>';
                 echo '</div>';
             ?>
 
