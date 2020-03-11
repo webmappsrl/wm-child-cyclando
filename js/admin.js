a5ba608faf59ab279b9744d1fdb96461c33abf78
Marco Baroncini <spartaco4404@yahoo.it>
2020-03-11 18:31:57 +0100

    fixed a bug, reactivate activity metabox in route post edit

--- js/admin.js
+++ js/admin.js
@@ -1,22 +1,22 @@
 jQuery(document).ready(function($){
 
+	// const $postRouteBody = $('.post-php.post-type-route');
+	// if ( $postRouteBody.length > 0 )
+	// {
+	// 	let removed = false;
+	// 	setInterval( () => {
+	// 		if ( ! removed )
+	// 		{
+	// 			console.log('test');
+	// 			let activitiesButton = $('.components-panel__body').find('button:contains(Activities)');
+	// 			if ( activitiesButton !== null && activitiesButton.length > 0 )
+	// 			{
+	// 				activitiesButton.parents('.components-panel__body').hide();
+	// 				removed=true;
+	// 			}
+	// 		}
 
-	const $postRouteBody = $('.post-php.post-type-route');
-	if ( $postRouteBody.length > 0 )
-	{
-		let removed = false;
-		setInterval( () => {
-			if ( ! removed )
-			{
-				console.log('test');
-				let activitiesButton = $('.components-panel__body').find('button:contains(Activities)');
-				if ( activitiesButton !== null && activitiesButton.length > 0 )
-				{
-					activitiesButton.parents('.components-panel__body').remove();
-					removed=true;
-				}
-			}
-
-		}, 500);
-	}
+	// 	}, 500);
+	// }
+	
 });
