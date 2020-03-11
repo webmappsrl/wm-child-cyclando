57aaf8b0dee23d3243c6076faddf9314d737d02d
Marco Baroncini <spartaco4404@yahoo.it>
2020-03-11 17:26:21 +0100

    fix a critical bug

--- shortcodes/dashboard_wizard_button.php
+++ shortcodes/dashboard_wizard_button.php
@@ -15,31 +15,11 @@ function wizard_button()
     <p>Crea una nuova route:</p>
     <?php if ($_SERVER['SERVER_NAME'] !== 'cyclando.com') { ?>
         <div id="wm-wizards-container">
-<<<<<<< HEAD
             <?php
             $conf = "routeWizard,singleFieldRouteWizard";
             echo do_shortcode ("[wmWizards conf='$conf']");
             ?>
         </div>
-    <?php } 
-=======
-        </div>
-    <?php } ?>
-
-    <script>
-        jQuery(document).ready(function($) {
-            const $routeWizard = $("#wm-wizards-container");
-            $.post(ajaxurl, {
-                action: 'getAngularApp',
-                name: 'wmWizards',
-                conf: 'routeWizard'
-            }, function(response) {
-                $routeWizard.html(response);
-            });
-        });
-    </script>
-<?php
-
->>>>>>> 7c1de4cdbbac69a184022149ecc7a97fbf339639
+    <?php }
     echo ob_get_clean();
 }
