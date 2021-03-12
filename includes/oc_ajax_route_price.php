49e7b1c732b103181a045b4aa00e007af617573f
Pedram Katanchi <pedramkat@gmail.com>
2021-03-12 14:43:18 +0100

    Added action to ajax calls for not logged in yousers

--- includes/oc_ajax_route_price.php
+++ includes/oc_ajax_route_price.php
@@ -1,6 +1,7 @@
 <?php 
 
 // action that process ajax call : webmapp_anypost-cy_route_advancedsearch-oneclick.php to update route price
+add_action( 'wp_ajax_nopriv_oc_ajax_route_price', 'oc_ajax_route_price' );
 add_action( 'wp_ajax_oc_ajax_route_price', 'oc_ajax_route_price' );
 function oc_ajax_route_price(){
     $cookies = $_POST['cookies'];
