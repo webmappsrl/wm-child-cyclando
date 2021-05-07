1d0f54f9da2c37d1dccc222058442cf391571a76
Pedram Katanchi <pedramkat@gmail.com>
2021-05-07 15:55:30 +0200

    Added the new extra labels

--- includes/wm_has_extra_get_label.php
+++ includes/wm_has_extra_get_label.php
@@ -109,6 +109,78 @@ function wm_has_extra_get_label($extra_variation_name,$extra_variation_desc) {
                 $label = __('Port charges (to be paid in advance)' ,'wm-child-verdenatura');
                 
     }
+
+    if('bike_plus' == $extra_variation_name) {           
+    
+                $label = __('Supplement for bike rental Premium' ,'wm-child-verdenatura');
+                
+    }
+
+    if('bike_pluswarranty' == $extra_variation_name) {           
+    
+                $label = __('Supplement for bike coverage Premium' ,'wm-child-verdenatura');
+                
+    }
+    if('bike_mtb' == $extra_variation_name) {           
+    
+                $label = __('Supplement for MTB rental' ,'wm-child-verdenatura');
+                
+    }
+    if('bike_mtbwarranty' == $extra_variation_name) {           
+    
+                $label = __('Supplement for tandem rental' ,'wm-child-verdenatura');
+                
+    }
+    if('bike_ebikemtb' == $extra_variation_name) {           
+    
+                $label = __('Supplemento nolo E-MTB' ,'wm-child-verdenatura');
+                
+    }
+    if('bike_ebikemtbwarranty' == $extra_variation_name) {           
+    
+                $label = __('Supplement for E-MTB coverage' ,'wm-child-verdenatura');
+                
+    }
+    if('bike_ebikeroad' == $extra_variation_name) {           
+    
+                $label = __('Supplement for road e-bike rental' ,'wm-child-verdenatura');
+                
+    }
+    if('bike_ecargo' == $extra_variation_name) {           
+    
+                $label = __('Supplement for ecargo rental' ,'wm-child-verdenatura');
+                
+    }
+    if('bike_ecargowarranty' == $extra_variation_name) {           
+    
+                $label = __('Supplement for ecago Coverage' ,'wm-child-verdenatura');
+                
+    }
+    if('bike_own' == $extra_variation_name) {           
+    
+                $label = __('Supplement for your own bike' ,'wm-child-verdenatura');
+                
+    }
+    if('bike_ownwarranty' == $extra_variation_name) {           
+    
+                $label = __('Supplement for own bike Coverage' ,'wm-child-verdenatura');
+                
+    }
+    if('bike_recumbent' == $extra_variation_name) {           
+    
+                $label = __('Supplement for recumbent bike' ,'wm-child-verdenatura');
+                
+    }
+    if('gps' == $extra_variation_name) {           
+    
+                $label = __('Supplement for GPS' ,'wm-child-verdenatura');
+                
+    }
+    if('weehoo' == $extra_variation_name) {           
+    
+                $label = __('Supplement for Weehoo trailer' ,'wm-child-verdenatura');
+                
+    }
     
     $name_explode = explode ('_',$extra_variation_name);
     if (!empty($name_explode) && $name_explode[0] == 'extra') {
