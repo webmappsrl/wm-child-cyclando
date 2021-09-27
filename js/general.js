2f18b33dd044ddb11a1d2a59e546b3b3169dd0ca
Pedram Katanchi <pedramkat@gmail.com>
2021-09-27 17:41:42 +0200

    fixed rank math cerca link in english 2

--- js/general.js
+++ js/general.js
@@ -71,6 +71,7 @@ jQuery(document).ready(function () {
   // Covid banner
   if (lang == "en-US") {
     jQuery("<div class='covidbanner'><div class='covidbanner-container'><span> NOTICE FOR TRAVELERS: <a href='https://cyclando.com/assicurazione-covid/'>find out more about COVID-19</a></span> <span class='cy-close-covidbanner'>&times;</span></div></div>").insertBefore(".l-canvas.type_wide");
+    jQuery('#searchpage-form-oneclick-mobile > div > div.searchpage-form-oneclick-body > div:nth-child(1) > div > input.facetwp-autocomplete.fcomplete-enabled').attr("placeholder","Select your destination");
   } else {
     jQuery("<div class='covidbanner'><div class='covidbanner-container'><span> AVVISO PER CHI VIAGGIA: <a href='https://cyclando.com/assicurazione-covid/'>scopri di più sul COVID-19</a></span> <span class='cy-close-covidbanner'>&times;</span></div></div>").insertBefore(".l-canvas.type_wide");
   }
@@ -109,7 +110,7 @@ jQuery(document).ready(function () {
     if (lang == "en-US") {
       // Dove vui andare?
       jQuery('#cy-search-element-container > div:nth-child(1) > div > input.facetwp-autocomplete.fcomplete-enabled').attr("placeholder","Select your destination");
-      jQuery('#searchpage-form-oneclick-mobile > div > div.searchpage-form-oneclick-body > div:nth-child(1) > div > input.facetwp-autocomplete.fcomplete-enabled').attr("placeholder","Choose a location");
+      jQuery('#searchpage-form-oneclick-mobile > div > div.searchpage-form-oneclick-body > div:nth-child(1) > div > input.facetwp-autocomplete.fcomplete-enabled').attr("placeholder","Select your destination");
       jQuery('#page-content > section.l-section.wpb_row.height_small.general-cerca-facetwp-container > div > div > div.vc_col-sm-12.wpb_column.vc_column_container.cerca-facets-container > div > div > div:nth-child(1) > div > div > div > div:nth-child(2) > div > div > div > div > input').attr("placeholder","Select your destination");
       // In quale mese?
       jQuery('#cy-search-element-container > div:nth-child(2) > div > select > option:nth-child(1)').text("Select month");
