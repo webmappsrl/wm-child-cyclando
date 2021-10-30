37bec6dfdf5dc72cabe014a7ea33d3517d326bee
Marco Baroncini <m.baroncini@netseven.it>
2021-10-30 17:40:07 +0200

    trying to fix facetwp index

--- wp-cli/wm-save-routes-cyc.php
+++ wp-cli/wm-save-routes-cyc.php
@@ -11,27 +11,24 @@
  *
  * @when after_wp_load
  */
-$wm_save_routes_cyc = function( $args, $assoc_args )
-{
+$wm_save_routes_cyc = function ($args, $assoc_args) {
     global $sitepress;
     $default_lang = $sitepress->get_default_language();
     $lang = '';
-    if ( !empty($args)) {
+    if (!empty($args)) {
         $sitepress->switch_lang($args[0]);
     }
 
-    $results = new WP_Query( array( 'post_type' => 'route','post_status' => 'publish','suppress_filters' => false, 'posts_per_page' => -1) );
-            
-            foreach ( $results->posts as $post ) {
-                WP_CLI::success('Updating Route ID # ' . $post->ID);
-                // wp_update_post( $post );
-                sync_route_dates_with_when($post->ID,$post,true);
-            }
+    $results = new WP_Query(array('post_type' => 'route', 'post_status' => 'publish', 'suppress_filters' => false, 'posts_per_page' => -1));
+    //$results_posts = array_slice($results->posts, 0, 10);
+    foreach ($results->posts as $post) {
+        WP_CLI::success('Updating Route ID # ' . $post->ID);
+        // wp_update_post( $post );
+        sync_route_dates_with_when($post->ID, $post, true);
+    }
 
 
-    $sitepress->switch_lang( $default_lang);
-
-
+    $sitepress->switch_lang($default_lang);
 };
 
-WP_CLI::add_command( 'wm-save-routes-cyc', $wm_save_routes_cyc );
+WP_CLI::add_command('wm-save-routes-cyc', $wm_save_routes_cyc);
