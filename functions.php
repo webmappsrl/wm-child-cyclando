f0661b459b7e1a8ddc03592e467abdcadf77f595
Marco Baroncini <m.baroncini@netseven.it>
2022-03-07 18:24:39 +0100

    fixed untranslated months on search

--- functions.php
+++ functions.php
@@ -488,34 +488,34 @@ add_filter( 'facetwp_i18n', function( $string ) {
     return $string;
 });
 
-// add_filter( 'facetwp_facet_render_args', function( $args ) {
-//     if ( 'quando_vuoi_partire' == $args['facet']['name'] ) {
-//         $translations = [
-//             'Gennaio' => __( 'January', 'wm-child-cyclando' ),
-//             'Febbraio' => __( 'February', 'wm-child-cyclando' ),
-//             'Marzo' => __( 'March', 'wm-child-cyclando' ),
-//             'Aprile' => __( 'April', 'wm-child-cyclando' ),
-//             'Maggio' => __( 'May', 'wm-child-cyclando' ),
-//             'Giugno' => __( 'June', 'wm-child-cyclando' ),
-//             'Luglio' => __( 'July', 'wm-child-cyclando' ),
-//             'Agosto' => __( 'August', 'wm-child-cyclando' ),
-//             'Settembre' => __( 'September', 'wm-child-cyclando' ),
-//             'Ottobre' => __( 'October', 'wm-child-cyclando' ),
-//             'Novembre' => __( 'November', 'wm-child-cyclando' ),
-//             'Dicembre' => __( 'December', 'wm-child-cyclando' ),
-//         ];
+add_filter( 'facetwp_facet_render_args', function( $args ) {
+    if ( 'quando_vuoi_partire' == $args['facet']['name'] ) {
+        $translations = [
+            'Gennaio' => __( 'January', 'wm-child-cyclando' ),
+            'Febbraio' => __( 'February', 'wm-child-cyclando' ),
+            'Marzo' => __( 'March', 'wm-child-cyclando' ),
+            'Aprile' => __( 'April', 'wm-child-cyclando' ),
+            'Maggio' => __( 'May', 'wm-child-cyclando' ),
+            'Giugno' => __( 'June', 'wm-child-cyclando' ),
+            'Luglio' => __( 'July', 'wm-child-cyclando' ),
+            'Agosto' => __( 'August', 'wm-child-cyclando' ),
+            'Settembre' => __( 'September', 'wm-child-cyclando' ),
+            'Ottobre' => __( 'October', 'wm-child-cyclando' ),
+            'Novembre' => __( 'November', 'wm-child-cyclando' ),
+            'Dicembre' => __( 'December', 'wm-child-cyclando' ),
+        ];
 
-//         if ( ! empty( $args['values'] ) ) {
-//             foreach ( $args['values'] as $key => $val ) {
-//                 $display_value = $val['facet_display_value'];
-//                 if ( isset( $translations[ $display_value ] ) ) {
-//                     $args['values'][ $key ]['facet_display_value'] = $translations[ $display_value ];
-//                 }
-//             }
-//         }
-//     }
-//     return $args;
-// });
+        if ( ! empty( $args['values'] ) ) {
+            foreach ( $args['values'] as $key => $val ) {
+                $display_value = $val['facet_display_value'];
+                if ( isset( $translations[ $display_value ] ) ) {
+                    $args['values'][ $key ]['facet_display_value'] = $translations[ $display_value ];
+                }
+            }
+        }
+    }
+    return $args;
+});
 
 
 
